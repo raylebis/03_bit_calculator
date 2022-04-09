14645fe5f1842c2181a9ec4b7ce1da171fd6b5c5
raylebis <100240815+raylebis@users.noreply.github.com>
2022-04-09 15:18:29 +1200

    add extra file and integer text

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -1,3 +1,5 @@
+# functions go here
+
 # Puts series of symbols at the start and end of text (for emphasis)
 def statement_generator(text, decoration):
 
@@ -13,5 +15,55 @@ def statement_generator(text, decoration):
 
     return ""
 
+# checks user's choice is an integer, text or an image
+def user_choice():
+    
+    # Lists of valid responses
+    text_ok = ["text", "t", "txt"]
+    integer_ok = ["integer", "int", "#", "number"] 
+    image_ok = ["image", "img", "I", "pix", "picture", "pic", "p"]
+   
+    valid = False
+    while not valid:
+
+        # ask user for choice and change response to lowercase
+        response = input("File type (integer / text / image): ").lower()
+
+        # Checks for valid response and returns text, integer or image
+
+        if response in text_ok:
+            return "text"
+            
+        elif response in integer_ok:
+            return "integer"
+            
+        elif response in image_ok:
+            return "image"
+
+        elif response == "i":
+            want_integer = input("Press <enter> for an integer or any key for an image")
+            if want_integer == "":
+                return "Integer"
+            else:
+                return "image"
+            
+        else:
+            # if response is not valid, output an error
+            print("Please choose a valid file type!")
+            print()
+
+
 # Main Routine goes here
+
+# Heading
 statement_generator("look - stars", "*")
+
+# Display instructions if user has not used program before
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    # Ask the user for the file type
+    data_type = user_choice()
+    print("You chose", data_type)
